fd66b6855cf71e84b899a5715399de07ebe5a156
smriti-02 <smritibisht02@gmail.com>
2026-07-02 22:33:00 +0530

    Add files via upload

---
+++ Week 1/DSA/Exersice7/Financial.java
@@ -0,0 +1,28 @@
+class Financial{
+
+    public static double futureValue(double currentValue,
+                                     double growthRate,
+                                     int years) {
+
+        if (years == 0) {
+            return currentValue;
+        }
+
+   
+        return futureValue(currentValue, growthRate, years - 1)
+                * (1 + growthRate);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000; // Initial investment
+        double growthRate = 0.10;    // 10% annual growth
+        int years = 5;
+
+        double predictedValue =
+                futureValue(currentValue, growthRate, years);
+
+        System.out.println("Future Value after "
+                + years + " years = " + predictedValue);
+    }
+}
