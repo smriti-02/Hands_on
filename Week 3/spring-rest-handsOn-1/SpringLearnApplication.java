013e2c3f598e6b47c3e0b03e9bf7fd14718af039
smriti-02 <smritibisht02@gmail.com>
2026-07-11 01:16:59 +0530

    Add files via upload

---
+++ Week 3/spring-rest-handsOn-1/SpringLearnApplication.java
@@ -0,0 +1,42 @@
+package com.cognizant.springlearn;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) throws Exception {
+
+        LOGGER.info("START");
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayDate();
+
+        LOGGER.info("END");
+    }
+
+    public static void displayDate() throws Exception {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+
+        System.out.println(date);
+    }
+}
