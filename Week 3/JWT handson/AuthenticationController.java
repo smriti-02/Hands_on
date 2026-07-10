be43669e71e98369c36f3ff75447e3283b9646c2
smriti-02 <smritibisht02@gmail.com>
2026-07-11 01:39:09 +0530

    Add files via upload

---
+++ Week 3/JWT handson/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public String authenticate() {
+        return "Authentication Service";
+    }
+}
